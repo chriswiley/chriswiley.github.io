21ef637829a743182bf158b051be34bac3a82433
Chris Wiley <chris@genesis-webdesign.com>
2018-07-23 02:08:27 -0400

    refreshed files for possible rewrite

--- jason.js
+++
@@ -1,8 +0,0 @@
-for (var i=0; i <= 3; i++) {
-  msg = (i == 2) ? "Happy Birthday dear Jason" : "Happy Birthday to You!" ;
-  console.log(msg);
-}
-function() {
-  var ev = "happy";
-  console.log(ev);
-}
